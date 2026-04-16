a1350e61de2f1a1268a751a5fc6ded79a07bc06d
gabmats-creator <aguilera.gabriel@edu.unifil.br>
2026-04-15 21:22:57 -0300

    remoção de código comentado no repositório

--- backend/app/repositories/room_repo.py
+++ backend/app/repositories/room_repo.py
@@ -11,11 +11,6 @@ class RoomRepository:
     def __init__(self, database: AsyncSession) -> None:
         self.database_session = database
 
-    # @staticmethod
-    # async def get_all(db: AsyncSession):
-    #     result = await db.execute(select(Room))
-    #     return result.scalars().all()
-
     async def create_room(self, room: RoomCreate) -> int:
         statement = (
             insert(Room)
@@ -32,10 +27,7 @@ class RoomRepository:
             return room_id
 
         except IntegrityError:
-            # Se cair aqui, é porque a restrição UNIQUE do 'name' falhou
-            # 1. Faz o rollback para limpar a transação que falhou
             await self.database_session.rollback()
-            # 2. Lança o erro de negócio que o seu Router já sabe tratar (vai virar um erro HTTP 409)
             raise BusinessRuleError("Já existe uma sala com este nome.")
 
     async def get_rooms(self) -> list[Room]:
