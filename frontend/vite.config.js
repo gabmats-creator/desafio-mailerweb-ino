f48ea7a63ee1db2e70c453553cb194879ac9c8c7
gabmats-creator <aguilera.gabriel@edu.unifil.br>
2026-04-16 09:44:51 -0300

    criação do backend e atualização do docker

---
+++ frontend/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Permite que o Docker exponha a porta
+    port: 5173,
+    watch: {
+      usePolling: true, // Força o Hot Reload a funcionar mapeado via Volume
+    }
+  }
+})
