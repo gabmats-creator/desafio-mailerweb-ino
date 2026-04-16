9505d8604e7a251cd1894948463e4fb98bf10a22
gabmats-creator <aguilera.gabriel@edu.unifil.br>
2026-04-15 21:21:00 -0300

    ajuste de escrita nas rotas de salas

--- backend/app/api/routers/rooms.py
+++ backend/app/api/routers/rooms.py
@@ -11,26 +11,16 @@ from app.exceptions.exception_decorator import handle_route_errors
 router = APIRouter()
 
 
-# TODO colocar tratamento de erro em um decorator
+# TODO autenticar rotas
 @router.post("/", status_code=status.HTTP_201_CREATED)
 @handle_route_errors
 async def create_room(
     room: RoomCreate,
     db: AsyncSession = Depends(get_db),
 ) -> SuccessOutputResponse:
-    # try:
     service = RoomService(RoomRepository(database=db))
-    # user_id viria do token JWT (dependência de auth)
     room_id = await service.create_room(room)
-    # TODO retornar o id ao invés de coroutine
     return SuccessOutputResponse(message=f"Sala de ID {room_id} criada com sucesso")
-    # except ValueError as e:
-    #     raise HTTPException(status_code=409, detail=str(e))
-    # except Exception as e:
-    #     import traceback
-
-    #     traceback.print_exc()
-    #     raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
